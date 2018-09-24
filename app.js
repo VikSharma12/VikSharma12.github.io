5fb5acdbbc71d2e0bf7dc481c475d60963ef0281
Vik Sharma <shar6770@mylaurier.ca>
2018-09-24 03:55:22 -0400

    fix for menu button on mobile

--- app.js
+++ app.js
@@ -21,3 +21,12 @@ var main = function(){
     }  
 };
 $(document).ready(main);
+
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+} 
