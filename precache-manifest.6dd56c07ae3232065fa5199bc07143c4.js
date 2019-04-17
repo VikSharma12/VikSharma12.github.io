84ae6ae978e4e34e4b3bd12aee5a92fccb9956cf
Vik Sharma <shar6770@mylaurier.ca>
2019-04-17 05:53:50 -0400

    Updates

---
+++ precache-manifest.6dd56c07ae3232065fa5199bc07143c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4ea1f57a1f507e695c6",
+    "url": "/my-app/static/css/main.41f14b80.chunk.css"
+  },
+  {
+    "revision": "a4ea1f57a1f507e695c6",
+    "url": "/my-app/static/js/main.b2b70176.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/my-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e36dbe7bc2dcff15d741e4ba160d4f58",
+    "url": "/my-app/index.html"
+  }
+];
